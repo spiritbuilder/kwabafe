c7dd7c1f4ca5299cadbb607de49621a1caf2e8d1
spiritbuilder <olaifatimothy@gmail.com>
2022-03-12 18:04:56 +0100

    fix responsiveness

--- src/pages/RequestLoan.jsx
+++ src/pages/RequestLoan.jsx
@@ -53,7 +53,7 @@ const RequestLoan = () => {
   }, []);
 
   return (
-    <div className="  xl:w-1/3 lg:w-2/5 md:w-2/5 w-2/3 sm:w-2/3  p-6 self-center justify-self-center rounded-md border border-kwabapurplelight">
+    <div className="  xl:w-1/3 lg:w-2/5 md:w-2/5  sm:w-3/5 w-4/5   p-6 self-center justify-self-center rounded-md border border-kwabapurplelight">
       <div className="flex justify-between center">
         <div className=" text-kwabapurplelight font-bold">
           Hi {auth?.authentication?.fullname}
